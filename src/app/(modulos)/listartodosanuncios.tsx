9d5e5c3c86f010ffb114671f16d43b53f089f222
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-05 16:40:06 -0300

    chore: configuração listar todos anuncio

---
+++ src/app/(modulos)/listartodosanuncios.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import ImageTeste from "../../../public/715.webp";
+import ImageTeste2 from "../../../public/1.jpg";
+
+const ListarTodosAnuncios = () => {
+  return (
+    <>
+      <ul className="m-0-auto font-inter w-auto text-xs">
+        <li className="relative float-left m-0">
+          <a
+            href="#image-1"
+            className="relative z-0 block h-40 w-40 sm:h-43 sm:w-43 md:h-45 md:w-45 lg:h-51 lg:w-51"
+          >
+            <Image
+              alt="Teste"
+              src="https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_1.jpg"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority={false}
+              className="absolute h-full w-full"
+            />
+            <h3 className="absolute inset-x-0 bottom-0 z-10 h-11 overflow-hidden bg-white/80 p-2 wrap-break-word">
+              Mercadinho Familia
+            </h3>
+          </a>
+          <div
+            className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden bg-white p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
+            id="image-1"
+          >
+            <a href="#principal" className="absolute z-50">
+              x FECHAR
+            </a>
+            <Image
+              alt="Teste"
+              src={ImageTeste}
+              className="relative m-auto mt-32 block h-80 w-70 border-2 border-black"
+            />
+            <div>
+              <h3>
+                Versão TESTE <span>/ 1.0 /</span>
+              </h3>
+              <p>Seu anuncio aqui !</p>
+            </div>
+          </div>
+        </li>
+        <li className="relative float-left m-0">
+          <a
+            href="#image-2"
+            className="relative z-0 block h-40 w-40 sm:h-43 sm:w-43 md:h-45 md:w-45 lg:h-51 lg:w-51"
+          >
+            <Image
+              alt="Teste"
+              src={ImageTeste2}
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="absolute h-full w-full"
+            />
+            <h1 className="absolute inset-x-0 bottom-0 z-10 h-11 overflow-hidden bg-white/80 p-2 wrap-break-word">
+              Boné Preto ótima qualidade
+            </h1>
+          </a>
+          <div
+            className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden bg-white p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
+            id="image-2"
+          >
+            <a href="#principal" className="absolute z-50">
+              x FECHAR
+            </a>
+            <Image
+              alt="Teste"
+              src={ImageTeste2}
+              className="relative m-auto mt-32 block h-80 w-70 border-2 border-black"
+            />
+            <div>
+              <h3>
+                Versão TESTE <span>/ 1.0 /</span>
+              </h3>
+              <p>Seu anuncio aqui !</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ListarTodosAnuncios;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/logo.png";
-import ImageTeste from "../../public/715.webp";
-import ImageTeste2 from "../../public/1.jpg";
 import Saudacao from "./(componentes)/mensagensdosistema/saudacao";
 import BotaoAnunciar from "./(componentes)/botoes/anunciar";
+import ListarTodosAnuncios from "./(modulos)/listartodosanuncios";
 
 export default function Home() {
   return (
@@ -42,83 +41,7 @@ export default function Home() {
         </div>
       </header>
       <section className="h-screen w-auto overflow-auto md:overflow-scroll">
-        <ul className="m-0-auto font-inter w-auto text-xs">
-          <li className="relative float-left m-0">
-            <a
-              href="#image-1"
-              className="relative z-0 block h-40 w-40 sm:h-43 sm:w-43 md:h-45 md:w-45 lg:h-51 lg:w-51"
-            >
-              <Image
-                alt="Teste"
-                src="/715.webp"
-                width={0}
-                height={0}
-                sizes="100vw"
-                priority={false}
-                className="absolute h-full w-full"
-              />
-              <h3 className="absolute inset-x-0 bottom-0 z-10 h-11 overflow-hidden bg-white/80 p-2 wrap-break-word">
-                Mercadinho Familia
-              </h3>
-            </a>
-            <div
-              className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden bg-white p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
-              id="image-1"
-            >
-              <a href="#principal" className="absolute z-50">
-                x FECHAR
-              </a>
-              <Image
-                alt="Teste"
-                src={ImageTeste}
-                className="relative m-auto mt-32 block h-80 w-70 border-2 border-black"
-              />
-              <div>
-                <h3>
-                  Versão TESTE <span>/ 1.0 /</span>
-                </h3>
-                <p>Seu anuncio aqui !</p>
-              </div>
-            </div>
-          </li>
-          <li className="relative float-left m-0">
-            <a
-              href="#image-2"
-              className="relative z-0 block h-40 w-40 sm:h-43 sm:w-43 md:h-45 md:w-45 lg:h-51 lg:w-51"
-            >
-              <Image
-                alt="Teste"
-                src="/1.jpg"
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="absolute h-full w-full"
-              />
-              <h1 className="absolute inset-x-0 bottom-0 z-10 h-11 overflow-hidden bg-white/80 p-2 wrap-break-word">
-                Boné Preto ótima qualidade
-              </h1>
-            </a>
-            <div
-              className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden bg-white p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
-              id="image-2"
-            >
-              <a href="#principal" className="absolute z-50">
-                x FECHAR
-              </a>
-              <Image
-                alt="Teste"
-                src={ImageTeste2}
-                className="relative m-auto mt-32 block h-80 w-70 border-2 border-black"
-              />
-              <div>
-                <h3>
-                  Versão TESTE <span>/ 1.0 /</span>
-                </h3>
-                <p>Seu anuncio aqui !</p>
-              </div>
-            </div>
-          </li>
-        </ul>
+        <ListarTodosAnuncios />
       </section>
     </main>
   );
