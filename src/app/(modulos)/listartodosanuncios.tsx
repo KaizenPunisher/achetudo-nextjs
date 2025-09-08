b504e29d5f47587bb3f9237a398da798cf04434f
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-08 17:12:11 -0300

    chore: pré config tarja preço

--- src/app/(modulos)/listartodosanuncios.tsx
+++ src/app/(modulos)/listartodosanuncios.tsx
@@ -29,12 +29,16 @@ const ListarTodosAnuncios = () => {
             className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden border-2 border-black bg-yellow-300 p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
             id="image-1"
           >
-            <div className="relative m-auto block h-100 w-screen border-5 border-black pt-0 md:w-160">
+            <div className="relative m-auto block h-80 w-screen border-5 border-black pt-0 md:h-90 md:w-110">
               <figure className="absolute h-[100%] w-[100%] border-2 border-blue-500 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_1.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
               <figure className="absolute h-[100%] w-[100%] border-2 border-blue-500 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_2.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
               <figure className="absolute h-[100%] w-[100%] border-2 border-blue-500 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_3.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
             </div>
-            <div className="absolute top-80 left-15 h-50 w-70 bg-white p-5 md:left-130">
+            <div className="absolute top-70 left-15 h-50 w-70 bg-white p-1 md:top-80 md:left-135">
+              <figure className="fixed top-71 left-9 h-15 w-30 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/icones/tarja.svg')] bg-size-[100%_100%] bg-center bg-no-repeat p-4">
+                <span className="text-center text-white">R$100000</span>
+              </figure>
+              <div className="h-13 w-full border-2 border-black"></div>
               <h3>
                 Versão TESTE <span>/ 1.0 /</span>
               </h3>
