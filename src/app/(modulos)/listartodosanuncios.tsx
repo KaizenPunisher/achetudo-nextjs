3d3c2216c4385330fdea831da5bc8c2387f481f7
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-09 12:52:57 -0300

    chore: ajuste pagina de anuncio

--- src/app/(modulos)/listartodosanuncios.tsx
+++ src/app/(modulos)/listartodosanuncios.tsx
@@ -27,66 +27,71 @@ const ListarTodosAnuncios = () => {
             </h3>
           </a>
           <div
-            className="animate-fade-in-scale fixed top-0 left-0 -z-10 hidden h-0 w-0 overflow-hidden border-2 border-black bg-yellow-300 p-0 text-center opacity-0 target:z-40 target:block target:h-screen target:w-screen target:opacity-100"
+            className="animate-fade-in-scale fixed top-0 left-0 -z-10 box-border hidden h-0 w-0 overflow-hidden rounded-b-2xl bg-yellow-300 p-0 text-center opacity-0 target:z-40 target:block target:h-full target:w-screen target:opacity-100"
             id="image-1"
           >
-            <div className="relative m-auto block h-80 w-screen border-5 border-black pt-0 md:h-90 md:w-110">
-              <figure className="absolute h-[100%] w-[100%] bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_1.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
-              <figure className="animate-slider absolute h-[100%] w-[100%] bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_2.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat opacity-0 transition-opacity delay-4000"></figure>
-              <figure className="animate-slider absolute h-[100%] w-[100%] bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_3.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat opacity-0 transition-opacity delay-10000"></figure>
+            <div className="relative m-auto box-border block h-80 w-screen rounded-2xl pt-0 shadow-xl/30 md:h-90 md:w-110">
+              <figure className="absolute box-border h-[100%] w-[100%] rounded-b-2xl bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_1.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
+              <figure className="animate-slider absolute box-border h-[100%] w-[100%] rounded-b-2xl bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_2.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat opacity-0 transition-opacity delay-4000"></figure>
+              <figure className="animate-slider absolute box-border h-[100%] w-[100%] rounded-b-2xl bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/anuncio/d7026cdf-8c46-413e-8989-c15712e81520_3.jpg')] bg-size-[100%_100%] bg-center bg-no-repeat opacity-0 transition-opacity delay-10000"></figure>
             </div>
-            <section className="font-comfortaa absolute top-70 left-15 box-border h-auto w-70 rounded-2xl bg-white p-1 md:top-80 md:left-135">
+            <section className="font-comfortaa absolute top-70 left-15 box-border h-auto w-70 rounded-2xl bg-white p-3 md:top-80 md:left-135">
               <figure className="fixed top-71 left-9 h-15 w-30 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/icones/tarja.svg')] bg-size-[100%_100%] bg-center bg-no-repeat pt-3 md:top-81 md:left-130">
                 <span className="text-center text-base text-white">
                   R$500,00
                 </span>
               </figure>
-              <div className="h-10 w-full border-2 border-black pt-2 pl-10">
+              <div className="h-10 w-full pt-2 pl-10">
                 <strong>Sites Landing Page</strong>
               </div>
-              <div className="h-auto w-auto border-2 border-black p-1">
+              <div className="h-auto w-auto">
                 <h4>
                   Marque presença na internet com um site profissional. Apareça
                   no google sem esforço.
                 </h4>
               </div>
-              <div className="h-10 w-auto border-2 border-black">
+              <div className="h-10 w-auto">
                 <Link
                   href="https://wa.me/5511951235344"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <figure className="float-left h-10 w-10 border-2 border-black bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/icones/whatsapp.svg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
-                  <span className="float-left border-2 border-black pt-2 pl-2 text-green-700">
+                  <figure className="float-left h-10 w-10 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/icones/whatsapp.svg')] bg-size-[100%_100%] bg-center bg-no-repeat"></figure>
+                  <span className="float-left pt-3 pl-1 text-green-700">
                     <strong>Fale agora conosco no whatsapp</strong>
                   </span>
                   <span className="clear-both"></span>
                 </Link>
               </div>
-              <div className="h-7 w-auto border-2 border-black pt-1 pb-1">
+              <div className="h-7 w-auto pt-1 pb-1">
                 <strong className="float-left pr-1">Empresa</strong>
                 <span className="float-left text-gray-500">- Dio Santos</span>
                 <span className="clear-both"></span>
               </div>
-              <div className="h-7 w-auto border-2 border-black pt-1 pb-1">
+              <div className="h-7 w-auto pt-1 pb-1">
                 <strong className="float-left pr-1">Funcionamento</strong>
                 <span className="float-left text-gray-500">
                   - Das 9:00 às 18:00
                 </span>
                 <span className="clear-both"></span>
               </div>
-              <div className="h-auto w-auto border-4 border-black pt-1 pb-1">
-                <Link href="https://wa.me/5511951235344" target="_blank">
-                  <div className="box-border h-10 w-30 rounded-2xl bg-yellow-400 pt-3">
-                    <span className="m-auto">Visite nosso site</span>
+              <div className="float-start h-auto w-full pt-1 pb-1">
+                <Link
+                  href="https://wa.me/5511951235344"
+                  target="_blank"
+                  className="float-left"
+                >
+                  <div className="box-border h-10 w-25 rounded-2xl bg-yellow-400 pt-3">
+                    <span className="m-auto">Nosso site</span>
                   </div>
                 </Link>
+                <a href="#principal" className="float-right">
+                  <div className="box-border h-10 w-25 rounded-2xl bg-black pt-3 text-white">
+                    <span className="m-auto">Fechar</span>
+                  </div>
+                </a>
+                <span className="clear-both"></span>
               </div>
-
-              <p>Seu anuncio aqui !</p>
-              <a href="#principal" className="absolute z-50">
-                x FECHAR
-              </a>
             </section>
           </div>
         </li>
@@ -117,7 +122,7 @@ const ListarTodosAnuncios = () => {
             <Image
               alt="Teste"
               src={ImageTeste2}
-              className="relative m-auto mt-32 block h-80 w-70 border-2 border-black"
+              className="relative m-auto mt-32 block h-80 w-70"
             />
             <div>
               <h3>
