ebade09f10c1173294c1e03cc92660686e5ae48e
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-25 13:59:59 -0300

    chore: pagina diosantos

---
+++ src/app/(pages)/diosantos/page.tsx
@@ -0,0 +1,9 @@
+export default function DioSantos() {
+  return (
+    <>
+      <main className="relative h-screen w-screen border-3 border-black bg-white">
+        Teste
+      </main>
+    </>
+  );
+}
