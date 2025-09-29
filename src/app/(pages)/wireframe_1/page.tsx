b188826fd165a3330fd8d132249f968712220ab9
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-29 13:08:30 -0300

    chore: pagina diosantos

--- src/app/(pages)/wireframe_1/page.tsx
+++ src/app/(pages)/wireframe_1/page.tsx
@@ -1,21 +1,21 @@
-export default function DioSantos() {
+export default function WireFrame_1() {
   return (
     <>
-      <main className="relative top-0 right-0 left-0 m-auto h-screen w-screen border-4 border-black bg-white">
-        <header className="fixed top-0 right-0 left-0 m-auto bg-gray-800 p-2">
+      <main className="relative top-0 left-0 m-auto flex h-screen w-screen flex-col items-center justify-center bg-white">
+        <header className="sticky top-0 left-0 m-auto w-full bg-gray-800 p-2">
           <div className="flex justify-center">
             <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
           </div>
         </header>
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <div className="bg-gray-300">
+        <div className="static m-auto h-full w-auto bg-amber-200">
+          <div className="inline-block bg-gray-300">
             <h1 className="text-6xl font-bold">Dio Santos</h1>
             <p className="mt-4 text-center text-lg">
               Welcome to the Dio Santos page!
             </p>
           </div>
         </div>
-        <footer className="right-0 bottom-0 left-0 m-auto bg-gray-800 p-4">
+        <footer className="static bottom-0 left-0 m-auto w-full bg-gray-800 p-4">
           <div className="flex justify-center">
             <p className="text-white">
               © 2023 Dio Santos. All rights reserved.
