db82c6f5d8d6a41dc76c679669435a30e9f63c04
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-29 10:07:00 -0300

    chore: pagina diosantos

--- src/app/(pages)/diosantos/page.tsx
+++ src/app/(pages)/diosantos/page.tsx
@@ -1,8 +1,17 @@
 export default function DioSantos() {
   return (
     <>
-      <main className="relative h-screen w-screen border-3 border-black bg-white">
-        Teste
+      <main className="relative top-0 right-0 left-0 m-auto h-screen w-screen border-4 border-black bg-white">
+        <header className="absolute top-0 right-0 left-0 m-auto bg-gray-800">
+          <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
+        </header>
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+        </div>
+        <footer></footer>
       </main>
     </>
   );
