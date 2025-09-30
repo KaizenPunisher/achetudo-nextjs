0298c535239af5ea6b1593116c92eff032018361
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-30 10:53:02 -0300

    chore: pagina diosantos

--- src/app/(pages)/wireframe_1/page.tsx
+++ src/app/(pages)/wireframe_1/page.tsx
@@ -1,19 +1,73 @@
 export default function WireFrame_1() {
   return (
     <>
-      <main className="relative top-0 left-0 m-auto flex h-screen w-screen flex-col items-center justify-center bg-white">
-        <header className="sticky top-0 left-0 m-auto w-full bg-gray-800 p-2">
+      <main className="relative top-0 left-0 m-auto flex h-full w-screen flex-col items-center justify-center bg-white">
+        <header className="fixed top-0 left-0 z-20 m-auto w-full bg-gray-800 p-2">
           <div className="flex justify-center">
             <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
           </div>
         </header>
+        <div className="absolute top-0 left-0 z-10 h-200 w-200 border-4 border-black">
+          <figure className="h-100 w-100 bg-blue-500"></figure>
+        </div>
+        <div className="relative h-170 w-full bg-gray-600 lg:h-240">
+          <figure className="absolute h-full w-full bg-amber-600"></figure>
+          <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-red-700 opacity-0 transition-opacity delay-4000"></figure>
+          <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-green-700 opacity-0 transition-opacity delay-10000"></figure>
+        </div>
         <div className="static m-auto h-full w-auto bg-amber-200">
-          <div className="inline-block bg-gray-300">
-            <h1 className="text-6xl font-bold">Dio Santos</h1>
-            <p className="mt-4 text-center text-lg">
-              Welcome to the Dio Santos page!
-            </p>
-          </div>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
+          <h1 className="text-6xl font-bold">Dio Santos</h1>
+          <p className="mt-4 text-center text-lg">
+            Welcome to the Dio Santos page!
+          </p>
         </div>
         <footer className="static bottom-0 left-0 m-auto w-full bg-gray-800 p-4">
           <div className="flex justify-center">
