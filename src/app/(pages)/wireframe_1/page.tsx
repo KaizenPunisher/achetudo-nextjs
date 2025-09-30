1cbf2e1f42b41babd733d93fc43b787cb4b98794
Kaizen <oscar.gomes.junior@hotmail.com>
2025-09-30 13:11:50 -0300

    chore: wireframe_1

--- src/app/(pages)/wireframe_1/page.tsx
+++ src/app/(pages)/wireframe_1/page.tsx
@@ -1,16 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
 export default function WireFrame_1() {
+  useEffect(() => {
+    const rolagem = () => {
+      const value = window.scrollY;
+      const slider = document.getElementById("slider") as HTMLElement;
+      if (slider) slider.style.height = `${value * -1}px`;
+      console.log(value);
+    };
+
+    window.addEventListener("scroll", rolagem);
+  }, []);
+
   return (
     <>
       <main className="relative top-0 left-0 m-auto flex h-full w-screen flex-col items-center justify-center bg-white">
-        <header className="fixed top-0 left-0 z-20 m-auto w-full bg-gray-800 p-2">
+        <header className="fixed top-0 left-0 z-20 m-auto w-full bg-gray-800 p-4">
           <div className="flex justify-center">
             <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
           </div>
         </header>
-        <div className="absolute top-0 left-0 z-10 h-200 w-200 border-4 border-black">
-          <figure className="h-100 w-100 bg-blue-500"></figure>
+        <div className="absolute top-0 left-0 z-10 h-60 w-50 border-4 border-black pt-20">
+          <figure className="h-30 w-30 bg-blue-500"></figure>
         </div>
-        <div className="relative h-170 w-full bg-gray-600 lg:h-240">
+        <div className="relative h-160 w-full bg-gray-600 lg:h-240" id="slider">
           <figure className="absolute h-full w-full bg-amber-600"></figure>
           <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-red-700 opacity-0 transition-opacity delay-4000"></figure>
           <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-green-700 opacity-0 transition-opacity delay-10000"></figure>
