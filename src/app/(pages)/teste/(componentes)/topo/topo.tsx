5bb112781da456ce1eb5812b70f069640e876caa
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-08 01:26:46 -0300

    refactor: Refatorando landing page Ache Tudo

---
+++ src/app/(pages)/teste/(componentes)/topo/topo.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function Topo() {
+  return (
+    <header className="fixed top-0 left-0 z-20 m-auto w-full bg-gray-800 p-4">
+      <div className="flex justify-center">
+        <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
+      </div>
+    </header>
+  );
+}
--- src/app/(pages)/teste/page.tsx
+++ src/app/(pages)/teste/page.tsx
@@ -1,12 +1,67 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import MetaTeste from "./metadata";
+import { Metadata } from "next";
+import Topo from "./(componentes)/topo/topo";
+/*
+export function generateMetadata(): Metadata {
+  return {
+    description:
+      "Pagina de teste para desenvolvimento de novas funcionalidades.",
+    title: "Pagina de Teste 1",
+    openGraph: {
+      title: "Pagina de Teste_1",
+      images: [
+        {
+          url: "https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/thumbs/thumb2.jpg", // O caminho para a imagem Open Graph
+          width: 1200,
+          height: 630,
+        },
+      ],
+      siteName: "Site name TESTE",
+      type: "website", // ou 'article', 'book', etc.
+      url: "https://www.achetudotiradentes.com.br/teste",
+    },
+    generator: "Next.js",
+    keywords: [
+      "Ache Tudo",
+      "Cidade Tiradentes",
+      "Ache Tudo Cidade Tiradentes",
+      "Ache Tudo Tiradentes",
+      "Comércio Cidade Tiradentes",
+      "Comércio Tiradentes",
+      "Ache Tudo e Região",
+    ],
 
+    authors: [
+      {
+        name: "Kaizen",
+        url: "https://github.com/KaizenPunisher/",
+      },
+    ],
+
+    icons: [
+      { rel: "apple-touch-icon", url: "icon-128x128.png" },
+      { rel: "icon", url: "icon-128x128.png" },
+    ],
+
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
+*/
 export default function Teste() {
   const [animateRodape, setAnimateRodape] = useState("");
 
-  MetaTeste();
   useEffect(() => {
     const rolagem = () => {
       const value = window.scrollY;
@@ -40,11 +95,7 @@ export default function Teste() {
         className="relative top-0 left-0 m-auto flex h-full w-screen flex-col items-center justify-center bg-white bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/4570f0bd-125f-4319-846e-9e263ffa66ee/slide_1.jpg')] bg-size-[100%_100%] bg-position-[0rem_0rem] bg-no-repeat"
         id="main"
       >
-        <header className="fixed top-0 left-0 z-20 m-auto w-full bg-gray-800 p-4">
-          <div className="flex justify-center">
-            <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
-          </div>
-        </header>
+        <Topo />
         <div
           className="fixed top-0 left-0 z-10 h-60 w-50 border-4 border-black pt-20"
           id="chamada_1"
