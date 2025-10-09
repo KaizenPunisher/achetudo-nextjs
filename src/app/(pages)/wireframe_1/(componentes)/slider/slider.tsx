3873b957a6c056cc74e3c7f78a43f7f626050e37
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-09 16:49:44 -0300

    refactor: Refatorando landing page Ache Tudo

--- src/app/(pages)/wireframe_1/(componentes)/slider/slider.tsx
+++ src/app/(pages)/wireframe_1/(componentes)/slider/slider.tsx
@@ -16,7 +16,10 @@ export default function Slider() {
     window.addEventListener("scroll", rolagem);
   }, []);
   return (
-    <div className="relative h-160 w-full bg-gray-600 lg:h-240" id="slider">
+    <div
+      className="relative m-0 h-160 w-full bg-gray-600 transition-all duration-100 lg:h-240"
+      id="slider"
+    >
       <figure className="absolute h-full w-full bg-amber-600"></figure>
       <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-red-700 opacity-0 transition-opacity delay-4000"></figure>
       <figure className="animate-slider absolute top-0 left-0 h-full w-full bg-green-700 opacity-0 transition-opacity delay-10000"></figure>
