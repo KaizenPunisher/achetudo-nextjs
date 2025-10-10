db7bfd54c155e024910903290b68abaf9b2b490e
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-10 16:18:07 -0300

    refactor: Refatorando landing page Ache Tudo

--- src/app/(pages)/teste/(componentes)/banner1/banner1.tsx
+++ src/app/(pages)/teste/(componentes)/banner1/banner1.tsx
@@ -19,7 +19,7 @@ export default function Banner1() {
       className="fixed top-0 left-0 z-10 m-0 h-60 w-[73vw] border-4 border-black pt-50 transition-all duration-100 lg:w-[55vw]"
       id="banner_1"
     >
-      <figure className="float-right h-50 w-40 bg-blue-500"></figure>
+      <figure className="z-20 float-right h-50 w-29 bg-blue-500 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/4570f0bd-125f-4319-846e-9e263ffa66ee/celular.png')] bg-size-[100%_100%] bg-position-[0rem_0rem] bg-no-repeat"></figure>
       <div className="clear-both"></div>
     </div>
   );
