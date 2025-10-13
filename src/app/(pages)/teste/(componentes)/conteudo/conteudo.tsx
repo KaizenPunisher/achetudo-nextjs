7e730ff43e3ef24525b8f50507d0a0161027ce32
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-13 16:34:19 -0300

    refactor: Refatorando landing page Ache Tudo

--- src/app/(pages)/teste/(componentes)/conteudo/conteudo.tsx
+++ src/app/(pages)/teste/(componentes)/conteudo/conteudo.tsx
@@ -2,7 +2,7 @@
 
 export default function Conteudo() {
   return (
-    <div className="static m-0 h-full w-auto pb-35">
+    <div className="static m-0 h-full w-auto pb-25">
       <div className="m-auto p-6 text-center">
         <span className="text-2xl font-bold text-blue-950 text-shadow-lg">
           Quais as vantagens em ter um site ?
@@ -22,6 +22,7 @@ export default function Conteudo() {
       <div className="m-auto mb-5 h-60 w-60 justify-center rounded-3xl border-1 border-white p-10 text-center text-xl backdrop-blur-sm">
         <span>Mostre sua loja ou produto nas redes sociais usando o site.</span>
       </div>
+      <div className="z-[-1] clear-both m-auto h-50 w-50 bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/4570f0bd-125f-4319-846e-9e263ffa66ee/fundo_chamada_2.png')] bg-size-[100%_100%] bg-center bg-no-repeat"></div>
     </div>
   );
 }
