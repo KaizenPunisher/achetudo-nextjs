765fa69e520cae97aa91ee6cb484f3423c2ac782
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-16 22:39:03 -0300

    chore: Configuração estrutura Dio Santos Landingpage

--- src/app/(pages)/diosantos/page.tsx
+++ src/app/(pages)/diosantos/page.tsx
@@ -1,27 +1,72 @@
+import { Metadata } from "next";
+import Topo from "./(componentes)/topo/topo";
+import Banner1 from "./(componentes)/banner1/banner1";
+import Slider from "./(componentes)/slider/slider";
+import Conteudo from "./(componentes)/conteudo/conteudo";
+import Rodape from "./(componentes)/rodape/rodape";
+
+export function generateMetadata(): Metadata {
+  return {
+    description: "Site Landing Page profissional com o menor preço do mercado.",
+    title: "Dio Santos Criação de Sites Landingpages",
+    openGraph: {
+      title: "Dio Santos Criação de Sites Landingpages",
+      images: [
+        {
+          url: "https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/a61efd63-20d5-464d-86af-8fb1183eee41/thumbs/thumb.jpg", // O caminho para a imagem Open Graph
+          width: 1200,
+          height: 630,
+        },
+      ],
+      siteName: "Dio Santos Criação de Sites Landingpages",
+      type: "website", // ou 'article', 'book', etc.
+      url: "https://www.achetudotiradentes.com.br/teste",
+    },
+    generator: "Next.js",
+    keywords: [
+      "Dio Santos Criação de Sites Landingpages",
+      "Design de Sites",
+      "Design de Landingpages",
+      "Design de Sites Profissionais",
+      "Design digital",
+      "Criação de Sistemas Web",
+    ],
+    authors: [
+      {
+        name: "Kaizen",
+        url: "https://github.com/KaizenPunisher/",
+      },
+    ],
+    icons: [
+      { rel: "apple-touch-icon", url: "icon-128x128.png" },
+      { rel: "icon", url: "icon-128x128.png" },
+    ],
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
+
 export default function DioSantos() {
   return (
     <>
-      <main className="relative top-0 right-0 left-0 m-auto h-screen w-screen border-4 border-black bg-white">
-        <header className="fixed top-0 right-0 left-0 m-auto bg-gray-800 p-2">
-          <div className="flex justify-center">
-            <h1 className="text-2xl font-bold text-white">Dio Santos</h1>
-          </div>
-        </header>
-        <div className="flex h-full w-full flex-col items-center justify-center">
-          <div className="bg-gray-300">
-            <h1 className="text-6xl font-bold">Dio Santos</h1>
-            <p className="mt-4 text-center text-lg">
-              Welcome to the Dio Santos page!
-            </p>
-          </div>
-        </div>
-        <footer className="right-0 bottom-0 left-0 m-auto bg-gray-800 p-4">
-          <div className="flex justify-center">
-            <p className="text-white">
-              © 2023 Dio Santos. All rights reserved.
-            </p>
-          </div>
-        </footer>
+      <main
+        className="relative top-0 left-0 m-0 flex h-full w-screen flex-col items-center justify-center bg-white bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/4570f0bd-125f-4319-846e-9e263ffa66ee/slide_1.jpg')] bg-size-[100%_100%] bg-position-[0rem_0rem] bg-no-repeat"
+        id="main"
+      >
+        <Topo />
+        <Banner1 />
+        <Slider />
+        <Conteudo />
+        <Rodape />
       </main>
     </>
   );
