ef2fa0ea03af31697961e763404da9aec5f807a6
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-20 16:36:53 -0300

    chore: Configuração estrutura Dio Santos Landingpage

--- src/app/(pages)/diosantos/(componentes)/slider/slider.tsx
+++ src/app/(pages)/diosantos/(componentes)/slider/slider.tsx
@@ -18,10 +18,10 @@ export default function Slider() {
   return (
     <>
       <div
-        className="relative m-0 h-160 w-full transition-all duration-100 lg:h-240"
+        className="relative m-0 h-100 w-full transition-all duration-100 lg:h-240"
         id="slider"
       >
-        <figure className="absolute h-full w-full"></figure>
+        <figure className="absolute h-full w-full bg-[url('https://achetudotiradentes-nextjs-app.s3.sa-east-1.amazonaws.com/pages/a61efd63-20d5-464d-86af-8fb1183eee41/slide_1.png')] bg-size-[100%_100%] bg-position-[0rem_0rem] bg-no-repeat"></figure>
         <figure className="animate-slider absolute top-0 left-0 h-full w-full opacity-0 transition-opacity delay-4000"></figure>
         <figure className="animate-slider absolute top-0 left-0 h-full w-full opacity-0 transition-opacity delay-10000"></figure>
       </div>
