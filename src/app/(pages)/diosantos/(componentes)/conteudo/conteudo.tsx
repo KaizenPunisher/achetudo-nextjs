017d0dac0f60f808ee304beedbb558f569e5f7a5
Kaizen <oscar.gomes.junior@hotmail.com>
2025-10-28 12:51:14 -0300

    chore: Configuração estrutura Dio Santos Landingpage

--- src/app/(pages)/diosantos/(componentes)/conteudo/conteudo.tsx
+++ src/app/(pages)/diosantos/(componentes)/conteudo/conteudo.tsx
@@ -1,59 +1,41 @@
 "use client";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Conteudo() {
   return (
     <>
       <div className="static m-0 h-full w-auto bg-amber-200 pb-35">
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">Dio Santos</h1>
-        <p className="mt-4 text-center text-lg">
-          Welcome to the Dio Santos page!
-        </p>
-        <h1 className="text-6xl font-bold">FINAL</h1>
-        <p className="mt-4 text-center text-lg">FINAL!</p>
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-sm"
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-3xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </>
   );
